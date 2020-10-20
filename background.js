c2e716b9c47bfb3e4e2a56375ff3a079ba9f9ed9
igorlogius <67047467+igorlogius@users.noreply.github.com>
2020-10-21 00:32:30 +0200

    cleanup

--- background.js
+++ background.js
@@ -1,42 +1,29 @@
 
-//add listener to browser button
-browser.browserAction.onClicked.addListener(async function(tab) {
+//add listener 
+browser.browserAction.onClicked.addListener(async (tab) => {
 
-	// gen foldername
+	// get timestamp
 	const now = new Date();
 	const date = now.getFullYear()+'-'+(now.getMonth()+1)+'-'+now.getDate();
 	const time = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
-	const dateTime = date+' '+time;
+	const dateTime = date + ' ' + time;
 
+	// use timestamp as foldername
 	const foldername = dateTime;
 
-	//console.log(foldername);
-
-	if(foldername === '') {return;}
-
-	const treenode = await browser.bookmarks.create({
-		'title': foldername
+	// create folder
+	const treenode = await browser.bookmarks.create({ 
+		'title': foldername 
 	});
 
 	//on click get all open tabs
 	const tabs = await browser.tabs.query({});
 
-	tabs.forEach(function(tab) {
-
-		//enclosed return function
-		var searchRet = (function(page){
-			return (function(res){
-				//save page only if not exist
-				if(res.length < 1){
-					console.log(page.title, page.url);
-					browser.bookmarks.create({'parentId': treenode.id,
-						'title': page.title, 'url': page.url});
-				}
-			});
-		})(tab);
-
-		//search each url if already exist
-		browser.bookmarks.search(tab.url, searchRet);
+	tabs.forEach( (tab) => {
+		browser.bookmarks.create({
+			'parentId': treenode.id
+			,'url': tab.url
+		});
 	});
-
 });
+
