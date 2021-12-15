b29f063596b5183b755d4dcbacf5bbfd427c5b5c
igorlogius <67047467+igorlogius@users.noreply.github.com>
2021-12-15 12:19:38 +0100

    feat: add configurable shortcut option feat: filter tabs via query() match pattern fix: prevent creation of empty folders per default

--- background.js
+++ background.js
@@ -17,10 +17,25 @@ function getTimeStampStr() {
 	return YY+'-'+MM+'-'+DD+' '+hh+":"+mm+":"+ss;
 }
 
-//add listener 
-browser.browserAction.onClicked.addListener(async (tab) => {
+async function save() {
+	// get open tabs
+	const tabs = await (async () => {
+		try {
+			return await browser.tabs.query({
+                hidden: false,
+                currentWindow: true,
+                url: ["http://*/*", "https://*/*"]
+            });
+		}catch(error){
+			console.log(error);
+			return null;
+		}
+	})();
+	if(tabs === null){return;}
+    if(tabs.length < 1){return;}
+
 	// create folder
-	const treenode = await (async ()=> { 
+	const treenode = await (async ()=> {
 		try {
 			return await browser.bookmarks.create({'title': getTimeStampStr() });
 		}catch(error){
@@ -29,26 +44,26 @@ browser.browserAction.onClicked.addListener(async (tab) => {
 		}
 	})();
 	if (treenode === null){return;}
-	// get open tabs
-	const tabs = await (async () => { 
-		try {
-			return await browser.tabs.query({hidden: false, currentWindow: true});
-		}catch(error){
-			console.log(error);
-			return null;
-		}
-	})();
-	if(tabs === null){return;}
 	// save each tab into the created folder
 	tabs.forEach( async (tab) => {
 		// only include http and https pages
-		if( /^https?:\/\//.test(tab.url) ){
+		//if( /^https?:\/\//.test(tab.url) ){
 			await browser.bookmarks.create({
 				'parentId': treenode.id
 				,'url': tab.url
 			});
-		}
+		//}
 	});
 	//
-});
+}
+//add listener
+browser.browserAction.onClicked.addListener(save);
 
+/*
+browser.menus.create({
+  id: 'save-tabs-to-bookmark-folder',
+  title: 'Save tabs to bookmark folder',
+  contexts: ["all"],
+  onclick: save
+});
+*/
