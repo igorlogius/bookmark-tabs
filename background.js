1bec88f7db732cc4ea6f8dc6f6254cf693276c62
igorlogius <67047467+igorlogius@users.noreply.github.com>
2023-03-17 11:48:16 +0100

    chore: cleanup & minor fixes & rename

--- background.js
+++ background.js
@@ -4,132 +4,142 @@ const manifest = browser.runtime.getManifest();
 const extname = manifest.name;
 let multipleHighlighted = false;
 
-async function showNotification(title,message){
-        const options = {
-                "type": "basic",
-                "iconUrl": browser.runtime.getURL("icon.png"),
-                "title": "Save Tabs to Bookmark Folder",
-                "message": message
-        };
-        try {
-                const nID = await browser.notifications.create(extname, options);
-                setTimeout(() => {
-                    browser.notifications.clear(nID);
-                },6*1000);
-        }catch(err){
-                console.error(err);
-        }
-        return null;
+async function showNotification(title, message) {
+  const options = {
+    type: "basic",
+    iconUrl: browser.runtime.getURL("icon.png"),
+    title: "Bookmark Tabs",
+    message: message,
+  };
+  try {
+    const nID = await browser.notifications.create(extname, options);
+    setTimeout(() => {
+      browser.notifications.clear(nID);
+    }, 6 * 1000);
+  } catch (err) {
+    console.error(err);
+  }
+  return null;
 }
 
-async function getFromStorage(storeid,fallback) {
-	return (await (async () => {
-		try {
-			let tmp = await browser.storage.local.get(storeid);
-			if (typeof tmp[storeid] !== 'undefined'){
-				return tmp[storeid];
-
-			}
-        }catch(e){
-			console.error(e);
-		}
-		return fallback;
-	})());
+async function getFromStorage(storeid, fallback) {
+  return await (async () => {
+    try {
+      let tmp = await browser.storage.local.get(storeid);
+      if (typeof tmp[storeid] !== "undefined") {
+        return tmp[storeid];
+      }
+    } catch (e) {
+      console.error(e);
+    }
+    return fallback;
+  })();
 }
 
 // add zero padding
-function pad (val) { return ((val < 10)?"0":"") + val; }
+function pad(val) {
+  return (val < 10 ? "0" : "") + val;
+}
 
 function getTimeStampStr() {
+  const now = new Date();
 
-	const now = new Date();
+  const YY = now.getFullYear();
+  const MM = pad(now.getMonth() + 1);
+  const DD = pad(now.getDate());
 
-	const YY = now.getFullYear();
-	const MM = pad((now.getMonth()+1));
-	const DD = pad(now.getDate());
+  const hh = pad(now.getHours());
+  const mm = pad(now.getMinutes());
+  const ss = pad(now.getSeconds());
 
-	const hh = pad(now.getHours());
-	const mm = pad(now.getMinutes());
-	const ss = pad(now.getSeconds());
-
-	return YY+'-'+MM+'-'+DD+' '+hh+":"+mm+":"+ss;
+  return YY + "-" + MM + "-" + DD + " " + hh + ":" + mm + ":" + ss;
 }
 
 async function save() {
+  const tabs = await (async () => {
+    try {
+      let queryObj = {
+        url: ["http://*/*", "https://*/*"],
+        currentWindow: true,
+        hidden: false,
+      };
+      if (multipleHighlighted) {
+        queryObj["highlighted"] = true;
+      }
+      return await browser.tabs.query(queryObj);
+    } catch (e) {
+      console.error(e);
+      return null;
+    }
+  })();
 
-    const tabs = await (async () => {
-        try {
-            let queryObj = {
-                "url": ["http://*/*", "https://*/*"],
-                "currentWindow": true
-            };
-            if(multipleHighlighted) {
-                queryObj["highlighted"] = true;
-            }
-            return await browser.tabs.query(queryObj);
-        }catch(e){
-            console.error(e);
-            return null;
-        }
-    })();
+  if (tabs === null) {
+    return 0;
+  }
+  if (tabs.length < 1) {
+    return 0;
+  }
 
-    if(tabs === null){return 0;}
-    if(tabs.length < 1){return 0;}
+  // get or create parent save folder
+  const saveFolderBM = await (async () => {
+    const saveFolder = await getFromStorage("saveFolder", "Saved Tabs");
+    // search
+    try {
+      const arr = await browser.bookmarks.search({ title: saveFolder });
+      if (arr.length > 0) {
+        return arr[0];
+      }
+    } catch (e) {
+      console.error(e);
+    }
+    // create
+    try {
+      return await browser.bookmarks.create({ title: saveFolder });
+    } catch (e) {
+      console.error(e);
+      return null;
+    }
+  })();
+  if (saveFolderBM === null) {
+    return 0;
+  }
 
-    // get or create parent save folder
-    const saveFolderBM = await (async ()=> {
-        const saveFolder = await getFromStorage('saveFolder', 'Saved Tabs')
-        // search
-        try {
-            const arr = await browser.bookmarks.search({'title': saveFolder });
-            if( arr.length > 0){
-                return arr[0];
-            }
-        }catch(e){
-            console.error(e);
-        }
-        // create
-        try {
-            return await browser.bookmarks.create({'title': saveFolder });
-        }catch(e){
-            console.error(e);
-            return null;
-        }
-    })();
-    if (saveFolderBM === null){return 0;}
+  // create timestamp save folder
+  let tsBM = await (async () => {
+    try {
+      return await browser.bookmarks.create({
+        parentId: saveFolderBM.id,
+        title: getTimeStampStr(),
+      });
+    } catch (e) {
+      console.error(e);
+      return null;
+    }
+  })();
+  if (tsBM === null) {
+    return 0;
+  }
 
-    // create timestamp save folder
-    let tsBM = await (async ()=> {
-        try {
-            return await browser.bookmarks.create({'parentId': saveFolderBM.id, 'title': getTimeStampStr() });
-        }catch(e){
-            console.error(e);
-            return null;
-        }
-    })();
-    if (tsBM === null){return 0;}
-
-    // save each tab into the created folder
-    tabs.forEach( async (tab) => {
-        await browser.bookmarks.create({
-            'parentId': tsBM.id
-            ,'url': tab.url
-        });
+  // save each tab into the created folder
+  tabs.forEach(async (tab) => {
+    await browser.bookmarks.create({
+      parentId: tsBM.id,
+      url: tab.url,
     });
-    // return amount of bookmarks
-    return tabs.length;
+  });
+  // return amount of bookmarks
+  return tabs.length;
 }
 
-async function saveAll(){
-    const nbtabs = await save();
-    showNotification("", "#Tabs Saved: " + nbtabs);
+async function saveAll() {
+  const nbtabs = await save();
+  showNotification("", "#Tabs Saved: " + nbtabs);
 }
 
 browser.browserAction.onClicked.addListener(saveAll);
 
 function handleHighlighted(highlightInfo) {
-    multipleHighlighted = (highlightInfo.tabIds.length > 1);
+  multipleHighlighted = highlightInfo.tabIds.length > 1;
 }
 
 browser.tabs.onHighlighted.addListener(handleHighlighted);
-
--- options.js
+++ options.js
@@ -1,56 +1,53 @@
 /* global browser */
 
 function onChange(evt) {
+  let id = evt.target.id;
+  let el = document.getElementById(id);
 
-	let id = evt.target.id;
-	let el = document.getElementById(id);
+  let value = el.type === "checkbox" ? el.checked : el.value;
+  let obj = {};
 
-	let value = ( (el.type === 'checkbox') ? el.checked : el.value)
-	let obj = {}
+  //console.log(id, value, el.type);
+  if (value === "") {
+    return;
+  }
+  if (el.type === "number") {
+    try {
+      value = parseInt(value);
+      if (isNaN(value)) {
+        value = el.min;
+      }
+      if (value < el.min) {
+        value = el.min;
+      }
+    } catch (e) {
+      value = el.min;
+    }
+  }
 
-	//console.log(id, value, el.type);
-	if(value === ""){
-		return;
-	}
-	if(el.type === 'number'){
-		try {
-			value = parseInt(value);
-			if(isNaN(value)){
-				value = el.min;
-			}
-			if(value < el.min) {
-				value = el.min;
-			}
-		}catch(e){
-			value = el.min
-		}
-	}
+  obj[id] = value;
 
-	obj[id] = value;
-
-	browser.storage.local.set(obj).catch(console.error);
+  browser.storage.local.set(obj).catch(console.error);
 }
 
-[ "saveFolder", "saveHidden"].map( (id) => {
+["saveFolder"].map((id) => {
+  browser.storage.local
+    .get(id)
+    .then((obj) => {
+      let el = document.getElementById(id);
+      let val = obj[id];
 
-	browser.storage.local.get(id).then( (obj) => {
+      if (typeof val !== "undefined") {
+        if (el.type === "checkbox") {
+          el.checked = val;
+        } else {
+          el.value = val;
+        }
+      }
+    })
+    .catch(console.error);
 
-		let el = document.getElementById(id);
-		let val = obj[id];
-
-		if(typeof val !== 'undefined') {
-			if(el.type === 'checkbox') {
-				el.checked = val;
-			}
-			else{
-				el.value = val;
-			}
-		}
-
-	}).catch( console.error );
-
-	let el = document.getElementById(id);
-	el.addEventListener('click', onChange);
-	el.addEventListener('keyup', onChange);
+  let el = document.getElementById(id);
+  el.addEventListener("click", onChange);
+  el.addEventListener("keyup", onChange);
 });
-
